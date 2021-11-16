6bb52fdfe68a895391190ffebd573591ffd47765
SkullZ1 <90234877+SkullZ1@users.noreply.github.com>
2021-11-16 11:06:44 +0700

    UTS

---
+++ GameUTS/Assets/script/GuiManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GuiManager : MonoBehaviour {
+
+
+
+	public void Onplay()
+	{
+		SceneManager.LoadScene("gameplay");
+	}
+	
+	public void OnPanduan()
+	{
+		SceneManager.LoadScene("panduan");
+	}
+	public void OnTentang()
+	{
+		SceneManager.LoadScene("tentang");
+	}
+	public void OnKeluar()
+	{
+		SceneManager.LoadScene("menu");
+	}
+	
+	
+}
